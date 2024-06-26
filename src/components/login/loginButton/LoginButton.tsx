520cea9a851784911e28c4b0b26aa2f30ba2420d
richardd3ng <rld39@duke.edu>
2024-06-26 13:57:21 -0700

    refactored login logic into hook

--- src/components/login/loginButton/LoginButton.tsx
+++ src/components/login/loginButton/LoginButton.tsx
@@ -1,71 +1,19 @@
-import { useEffect } from "react";
-
-import { useAuthRequest, DiscoveryDocument } from "expo-auth-session";
-import { Alert, Text } from "react-native";
-import { useNavigation } from "@react-navigation/native";
-import { SPOTIFY_CLIENT_ID, SPOTIFY_REDIRECT_URI } from "@env";
+import { Text } from "react-native";
 
 import createStyles from "./styles";
 import CustomPressable from "../../shared/customPressable/CustomPressable";
 import SpotifyIcon from "../spotifyIcon/SpotifyIcon";
-import { ScreenName, StackNavigation } from "../../../types/navigation";
+import useLogin from "../../../hooks/useLogin";
 import useThemeAwareObject from "../../../hooks/useThemeAwareObject";
-import { fetchAuthLogin } from "../../../api/authAPI";
-import { storeTokens } from "../../../utils/secureStoreUtils";
-import { dispatchSetCurrentUser } from "../../../state/storeUtils";
-
-const discovery: DiscoveryDocument = {
-    authorizationEndpoint: "https://accounts.spotify.com/authorize",
-    tokenEndpoint: "https://accounts.spotify.com/api/token",
-};
 
 const LoginButton = () => {
     const styles = useThemeAwareObject(createStyles);
-    const { navigate } = useNavigation<StackNavigation>();
-    const [request, response, promptAsync] = useAuthRequest(
-        {
-            clientId: SPOTIFY_CLIENT_ID,
-            scopes: [
-                "ugc-image-upload",
-                "playlist-modify-private",
-                "user-read-recently-played",
-                "user-read-private",
-            ],
-            redirectUri: SPOTIFY_REDIRECT_URI,
-            usePKCE: false,
-        },
-        discovery
-    );
-
-    useEffect(() => {
-        if (response?.type === "success") {
-            const { code, state } = response.params;
-            const login = async () => {
-                try {
-                    const { tokens, userInfo } = await fetchAuthLogin({
-                        code,
-                        state,
-                    });
-                    await storeTokens(
-                        tokens.accessToken,
-                        tokens.refreshToken,
-                        tokens.expiresIn
-                    );
-                    dispatchSetCurrentUser(userInfo);
-                    navigate(ScreenName.Map);
-                } catch (error) {
-                    console.error(error);
-                    Alert.alert("Error fetching access token");
-                }
-            };
-            login();
-        }
-    }, [response]);
+    const { promptLogin } = useLogin();
 
     return (
         <CustomPressable
             style={styles.button}
-            onPress={() => promptAsync()}
+            onPress={() => promptLogin()}
             activeOpacity={0.8}
         >
             <SpotifyIcon width={24} height={24} />
---
+++ src/hooks/useLogin.ts
@@ -0,0 +1,74 @@
+import { useEffect } from "react";
+
+import { Alert } from "react-native";
+import { useAuthRequest, DiscoveryDocument } from "expo-auth-session";
+import { useNavigation } from "@react-navigation/native";
+import { useSelector } from "react-redux";
+
+import { dispatchSetCurrentUser } from "../state/storeUtils";
+import { fetchAuthLogin } from "../api/authAPI";
+import { RootState } from "../state/store";
+import { ScreenName, StackNavigation } from "../types/navigation";
+import { SPOTIFY_CLIENT_ID, SPOTIFY_REDIRECT_URI } from "@env";
+import { storeTokens } from "../utils/secureStoreUtils";
+
+const discovery: DiscoveryDocument = {
+    authorizationEndpoint: "https://accounts.spotify.com/authorize",
+    tokenEndpoint: "https://accounts.spotify.com/api/token",
+};
+
+const useLogin = () => {
+    const { navigate } = useNavigation<StackNavigation>();
+    const currentUser = useSelector(
+        (state: RootState) => state.user.currentUser
+    );
+    const [_request, response, promptLogin] = useAuthRequest(
+        {
+            clientId: SPOTIFY_CLIENT_ID,
+            scopes: [
+                "ugc-image-upload",
+                "playlist-modify-private",
+                "user-read-recently-played",
+                "user-read-private",
+            ],
+            redirectUri: SPOTIFY_REDIRECT_URI,
+            usePKCE: false,
+        },
+        discovery
+    );
+
+    useEffect(() => {
+        if (currentUser) {
+            navigate(ScreenName.Map);
+        }
+    }, [currentUser, navigate]);
+
+    useEffect(() => {
+        if (response?.type === "success") {
+            const { code, state } = response.params;
+            const login = async () => {
+                try {
+                    const { tokens, userInfo } = await fetchAuthLogin({
+                        code,
+                        state,
+                    });
+                    await storeTokens(
+                        tokens.accessToken,
+                        tokens.refreshToken,
+                        tokens.expiresIn
+                    );
+                    dispatchSetCurrentUser(userInfo);
+                    navigate(ScreenName.Map);
+                } catch (error) {
+                    console.error(error);
+                    Alert.alert("Error fetching access token");
+                }
+            };
+            login();
+        }
+    }, [response, navigate]);
+
+    return { promptLogin };
+};
+
+export default useLogin;
