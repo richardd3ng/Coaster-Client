22d378adf3b669c5c952a863c70b5eee14114700
richardd3ng <rld39@duke.edu>
2024-06-28 00:09:55 -0700

    cleaned up codegen config

--- codegen.ts
+++ codegen.ts
@@ -2,10 +2,7 @@ import { CodegenConfig } from "@graphql-codegen/cli";
 
 const config: CodegenConfig = {
     schema: "http://localhost:3000/graphql",
-    documents: [
-        "src/**/*.{ts, tsx, graphql, gql}",
-        "src/**/*.fragment.graphql", // Include files containing GraphQL fragments
-    ],
+    documents: ["src/**/*.{ts, tsx}"],
     ignoreNoDocuments: true,
     generates: {
         "./src/gql/": {
